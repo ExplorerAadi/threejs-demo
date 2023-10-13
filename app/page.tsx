1cc3e6286d1b4c4b3153eabd2fc639a7fc3cfa7a
Aditya Raj <rajaditya27145@gmail.com>
2023-10-13 16:24:44 +0530

    feat: Rigid body with keyboard control

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,288 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import { Canvas, ThreeEvent, useFrame, useThree } from "@react-three/fiber";
+import {
+  Environment,
+  OrbitControls,
+  useAnimations,
+  useGLTF,
+} from "@react-three/drei";
+import { useState, useEffect, useRef, ReactNode } from "react";
+import * as THREE from "three";
+
+export default function Page() {
+  const [keysPressed, setKeysPressed] = useState<Record<string, boolean>>({});
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      setKeysPressed((keys) => ({ ...keys, [event.key]: true }));
+    };
+
+    const handleKeyUp = (event: KeyboardEvent) => {
+      setKeysPressed((keys) => ({ ...keys, [event.key]: false }));
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div className="h-screen flex items-center justify-center w-screen">
+      <Canvas camera={{ fov: 75 }} shadows>
+        {/* <gridHelper /> */}
+        <Experience>
+          <Model keysPressed={keysPressed} />
+        </Experience>
+      </Canvas>
+    </div>
+  );
 }
+
+function Model({ keysPressed }: { keysPressed: Record<string, boolean> }) {
+  const [animation, setAnimation] = useState<"Idle" | "Running">("Idle");
+  const group = useRef<any>(null);
+  const controlsRef = useRef<typeof OrbitControls>();
+  // @ts-expect-error
+  const { nodes, materials, animations } = useGLTF("/woman.gltf");
+  const { actions } = useAnimations(animations as any[], group);
+  const camera = useThree((state) => state.camera);
+  const walkDirection = new THREE.Vector3();
+  const rotateAngle = new THREE.Vector3(1, 0, 0);
+  // const rotateQuarternion = new THREE.Quaternion();
+  const cameraTarget = new THREE.Vector3();
+
+  useEffect(() => {
+    actions[animation]?.reset().play();
+    return () => {
+      actions[animation]?.fadeOut(0.5);
+    };
+  }, [actions, animation]);
+
+  const pressedKey = Object.keys(keysPressed).find((key) => keysPressed[key]);
+
+  const updateCameraTarget = (moveX: number, moveZ: number) => {
+    camera.position.x += moveX;
+    camera.position.z += moveZ;
+
+    cameraTarget.x = group.current.position.x;
+    cameraTarget.y = group.current.position.y + 2;
+    cameraTarget.z = group.current.position.z;
+    if (controlsRef.current) {
+      controlsRef.current.target = cameraTarget;
+    }
+  };
+
+  useFrame((_state, delta) => {
+    if (group.current) {
+      if (pressedKey) {
+        camera.getWorldDirection(walkDirection);
+        walkDirection.y = 0;
+        walkDirection.normalize();
+        walkDirection.applyAxisAngle(rotateAngle, group.current.rotation.y);
+
+        const moveX = walkDirection.x * delta;
+        const moveZ = walkDirection.z * delta;
+        updateCameraTarget(moveX, moveZ);
+
+        switch (pressedKey) {
+          case "ArrowRight":
+            setAnimation("Running");
+            group.current.rotation.y = -Math.PI / 2;
+            group.current.position.x += delta;
+            break;
+          case "ArrowLeft":
+            setAnimation("Running");
+            group.current.rotation.y = Math.PI / 2;
+            group.current.position.x -= 0.02;
+            break;
+          case "ArrowUp":
+            setAnimation("Running");
+            group.current.rotation.y = 0;
+            group.current.position.z -= 0.02;
+            break;
+          case "ArrowDown":
+            setAnimation("Running");
+            group.current.rotation.y = -Math.PI;
+            group.current.position.z += 0.02;
+            break;
+        }
+      } else {
+        setAnimation("Idle");
+      }
+    }
+  });
+
+  return (
+    <group ref={group} dispose={null} name="modelContainer">
+      <Environment files="/artist_workshop_1k.hdr" background />
+      <group name="Model">
+        <group
+          name="Armature"
+          rotation={[Math.PI / 2, 0, Math.PI]}
+          scale={0.01}
+        >
+          <OrbitControls ref={controlsRef} />
+          <primitive object={nodes.mixamorigHips} />
+          <group name="SM_Chr_Developer_Female_02">
+            <skinnedMesh
+              castShadow
+              name="Mesh019"
+              geometry={nodes.Mesh019.geometry}
+              material={materials.Glasses}
+              skeleton={nodes.Mesh019.skeleton}
+            />
+            <skinnedMesh
+              castShadow
+              name="Mesh019_1"
+              geometry={nodes.Mesh019_1.geometry}
+              material={materials.Eyes}
+              skeleton={nodes.Mesh019_1.skeleton}
+            />
+            <skinnedMesh
+              castShadow
+              name="Mesh019_2"
+              geometry={nodes.Mesh019_2.geometry}
+              material={materials.Hair}
+              skeleton={nodes.Mesh019_2.skeleton}
+            />
+            <skinnedMesh
+              castShadow
+              name="Mesh019_3"
+              geometry={nodes.Mesh019_3.geometry}
+              material={materials.Skin}
+              skeleton={nodes.Mesh019_3.skeleton}
+            />
+            <skinnedMesh
+              castShadow
+              name="Mesh019_4"
+              geometry={nodes.Mesh019_4.geometry}
+              material={materials.Mouth}
+              skeleton={nodes.Mesh019_4.skeleton}
+            />
+            <skinnedMesh
+              castShadow
+              name="Mesh019_5"
+              geometry={nodes.Mesh019_5.geometry}
+              material={materials.Shirt}
+              skeleton={nodes.Mesh019_5.skeleton}
+            />
+            <skinnedMesh
+              castShadow
+              name="Mesh019_6"
+              geometry={nodes.Mesh019_6.geometry}
+              material={materials.Pants}
+              skeleton={nodes.Mesh019_6.skeleton}
+            />
+            <skinnedMesh
+              castShadow
+              name="Mesh019_7"
+              geometry={nodes.Mesh019_7.geometry}
+              material={materials.Shoes}
+              skeleton={nodes.Mesh019_7.skeleton}
+            />
+            <skinnedMesh
+              castShadow
+              name="Mesh019_8"
+              geometry={nodes.Mesh019_8.geometry}
+              material={materials.Sole}
+              skeleton={nodes.Mesh019_8.skeleton}
+            />
+            <skinnedMesh
+              castShadow
+              name="Mesh019_9"
+              geometry={nodes.Mesh019_9.geometry}
+              material={materials.Laces}
+              skeleton={nodes.Mesh019_9.skeleton}
+            />
+          </group>
+        </group>
+      </group>
+    </group>
+  );
+}
+
+function Experience({ children }: { children: ReactNode }) {
+  const modelRef = useRef(null);
+  const rotateAxis = new THREE.Vector3(0, 1, 0);
+  const geometry = new THREE.PlaneGeometry(1, 2.5);
+  const material = new THREE.MeshBasicMaterial({
+    color: "#e0e0e0",
+    side: THREE.DoubleSide,
+  });
+  const door = new THREE.Mesh(geometry, material).rotateOnAxis(
+    rotateAxis,
+    Math.PI / 2
+  );
+  const stand = new THREE.Mesh(geometry, material).rotateOnAxis(
+    rotateAxis,
+    Math.PI / 2
+  );
+  door.name = "door";
+  stand.name = "stand";
+
+  const handleDoorClick = (event: ThreeEvent<MouseEvent>) => {
+    const modelScene = (modelRef.current as any).getObjectByName(
+      "modelContainer"
+    );
+    const proximity = event.point.distanceTo(
+      modelScene.position as THREE.Vector3
+    );
+
+    if (proximity < 1.2) {
+      console.log("Door Clicked");
+    }
+  };
+
+  const handleStandClick = (event: ThreeEvent<MouseEvent>) => {
+    const modelScene = (modelRef.current as any).getObjectByName(
+      "modelContainer"
+    );
+    const proximity = event.point.distanceTo(
+      modelScene.position as THREE.Vector3
+    );
+
+    if (proximity < 1.2) {
+      console.log("Wallet Clicked");
+    }
+  };
+
+  return (
+    <>
+      <OrbitControls />
+      <ambientLight />
+      <directionalLight
+        position={[-5, 5, 5]}
+        castShadow
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+      />
+      <group position={[5, 0, -6]} onClick={handleDoorClick}>
+        <primitive object={door} />
+      </group>
+      <group position={[0, -1, 0]} ref={modelRef}>
+        {children}
+      </group>
+      <group position={[-5, 0, -6]} onClick={handleStandClick}>
+        <primitive object={stand} />
+      </group>
+      <mesh
+        rotation={[-0.5 * Math.PI, 0, 0]}
+        position={[0, -1, 0]}
+        receiveShadow
+      >
+        <planeGeometry args={[10, 10, 1, 1]} />
+        <shadowMaterial transparent opacity={0.2} />
+      </mesh>
+    </>
+  );
+}
+
+useGLTF.preload("/woman.gltf");
+
+// TODO: Rotate character and camera as they move
+// Put some objects and trigger actions based on proximity
