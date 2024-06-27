5603cd50c313ebe6a9a30c98214e7f1a2508c035
Aditya Raj <rajaditya27145@gmail.com>
2024-06-28 04:12:36 +0530

    add custom shaders as portal

--- app/ascii/ASCIIEffect.tsx
+++ app/ascii/ASCIIEffect.tsx
@@ -11,16 +11,23 @@ import {
 
 import font from "./font.json";
 import { useRef } from "react";
-import { Mesh, Shape } from "three";
-import { EffectComposer } from "@react-three/postprocessing";
+import { Mesh, ShaderMaterial, Shape } from "three";
+// @ts-ignore
+import fragmentShader from "./shaders/fragment.glsl";
+// @ts-ignore
+import vertexShader from "./shaders/vertex.glsl";
 
 export const ASCIIEffect = () => {
   const { camera, gl } = useThree();
   const textRef = useRef<Mesh>(null);
+  const shaderRef = useRef<ShaderMaterial>(null);
 
   useFrame((_state, delta) => {
     if (textRef.current) {
-      textRef.current.position.x -= delta * 0.5;
+      textRef.current.position.x -= delta * 0.1;
+    }
+    if (shaderRef.current) {
+      shaderRef.current.uniforms.uTime.value += delta;
     }
   });
 
@@ -29,29 +36,37 @@ export const ASCIIEffect = () => {
       <OrbitControls args={[camera, gl.domElement]} />
 
       <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 10, 5]} intensity={1} />
-      <EffectComposer>
-        <AsciiRenderer characters=" .:,/-=+*#%1234567890@$" invert={false} />
-      </EffectComposer>
-
-      <Center position={[0, 0, 0]}>
-        <AnimatingObject />
-        <Text3D
-          position={[0, 0, 0]}
-          font={font as unknown as FontData}
-          size={6}
-          height={0.2}
-          letterSpacing={0.1}
-          curveSegments={32}
-          bevelEnabled
-          bevelSize={0.05}
-          bevelThickness={0.05}
-          ref={textRef}
-        >
-          methlab
-          <meshStandardMaterial attach="material" color="white" />
-        </Text3D>
-      </Center>
+      {/* <directionalLight position={[0, 10, 5]} intensity={1} /> */}
+      <AsciiRenderer characters=" .:,/-=+*#%@" invert={false} />
+      {/* <AnimatingObject /> */}
+      <mesh ref={textRef}>
+        <Center position={[0, -1, 0]}>
+          <Text3D
+            rotation={[0, 0.5, 0]}
+            font={font as unknown as FontData}
+            size={6}
+            height={0.2}
+            letterSpacing={0.1}
+            curveSegments={6}
+            bevelEnabled
+            bevelSegments={4}
+            bevelSize={0.05}
+            bevelThickness={0.05}
+          >
+            <shaderMaterial
+              ref={shaderRef}
+              attach="material"
+              uniforms={{
+                uTime: { value: 0 },
+              }}
+              vertexShader={vertexShader}
+              fragmentShader={fragmentShader}
+            />
+            methlab
+            {/* <meshStandardMaterial attach="material" color="white" /> */}
+          </Text3D>
+        </Center>
+      </mesh>
     </>
   );
 };
@@ -61,13 +76,18 @@ const AnimatingObject = () => {
 
   useFrame((_state, delta) => {
     if (meshRef.current) {
-      meshRef.current.position.x += delta * 0.5;
+      meshRef.current.position.x += delta * 1;
     }
   });
 
   return (
     <>
-      <mesh ref={meshRef} position={[0, 0, 1]} scale={[1.5, 1.5, 1.5]}>
+      <mesh
+        ref={meshRef}
+        position={[0, 0, 0]}
+        scale={[1.5, 1.5, 1.5]}
+        rotation={[0, 0.5, 0]}
+      >
         <extrudeGeometry
           args={[
             new Shape().moveTo(0, 0).lineTo(2, 3).lineTo(4, 0).lineTo(0, 0),
